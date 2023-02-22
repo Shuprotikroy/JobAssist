3de49dd4b829da60bff9738ed111fd8940b24513
Shuprotik Roy <70252750+Shuprotikroy@users.noreply.github.com>
2023-02-22 17:48:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -210,21 +210,3 @@ def output_text(value):
 ]            
 if __name__=="__main__":
     app.run_server(port=8050)
-
-# dbc.Card(
-#     [
-#         dbc.CardImg(src="/static/images/placeholder286x180.png", top=True),
-#         dbc.CardBody(
-#             [
-#                 html.H4("Card title", className="card-title"),
-#                 html.P(
-#                     "Some quick example text to build on the card title and "
-#                     "make up the bulk of the card's content.",
-#                     className="card-text",
-#                 ),
-#                 dbc.Button("Go somewhere", color="primary"),
-#             ]
-#         ),
-#     ],
-#     style={"width": "18rem"},
-# )
